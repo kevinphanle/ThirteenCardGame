7e977b605343e9f7660ee4a5729580375290b3ec
Kevin Phan Le <kevinphanle11@gmail.com>
2025-03-13 00:26:30 -0700

    created a handhistory component

--- src/PlayArea.tsx
+++ src/PlayArea.tsx
@@ -5,6 +5,7 @@ import classNames from "./App.module.css";
 import { useGameContext } from "./GameContext";
 import { getHandType } from "./HandTypeUtils";
 import Card from "./Card";
+import HandHistory from "./HandHistory";
 
 interface PlayAreaProps {
   playerHands: CardType[][];
@@ -63,104 +64,11 @@ const PlayArea: React.FC<PlayAreaProps> = ({ playerHands, onPlayCards }) => {
 
         {/* Center play area */}
         <div className={classNames.centerPlayArea}>
-          {currentHandType && (
-            <div className="current-hand-type">
-              Current Hand Type: {currentHandType}
-            </div>
-          )}
-
-          {handHistory.length > 0 && (
-            <div className={classNames.handHistoryContainer}>
-              <h3 onClick={() => setShowHistory(!showHistory)}>
-                Game History {showHistory ? "▼" : "▶"}
-              </h3>
-
-              {/* Current play - always visible */}
-              <div className={classNames.currentPlay}>
-                {(() => {
-                  // Find the last non-pass entry
-                  const lastNonPassEntry = [...handHistory]
-                    .reverse()
-                    .find((entry) => entry.cards.length > 0);
-
-                  if (lastNonPassEntry) {
-                    return (
-                      <>
-                        <div className={classNames.currentPlayInfo}>
-                          <span className={classNames.handType}>
-                            {lastNonPassEntry.handType}
-                          </span>
-                        </div>
-                        <div className={classNames.currentPlayCards}>
-                          {lastNonPassEntry.cards.map((card, cardIndex) => (
-                            <div
-                              className={classNames.currentCardContainer}
-                              key={cardIndex}
-                              style={{
-                                marginLeft: cardIndex > 0 ? "-40px" : "0",
-                              }}
-                            >
-                              <Card card={{ ...card, hidden: false }} />
-                            </div>
-                          ))}
-                        </div>
-                      </>
-                    );
-                  }
-                  return (
-                    <span className={classNames.noPlay}>
-                      No cards played yet
-                    </span>
-                  );
-                })()}
-              </div>
-
-              {/* Expanded history */}
-              {showHistory && (
-                <div className={classNames.historyTimeline}>
-                  {handHistory.map((entry, index) => (
-                    <div
-                      key={index}
-                      className={`${classNames.historyEntry} ${
-                        entry.handType === "Pass"
-                          ? classNames.passEntry
-                          : classNames.playEntry
-                      }`}
-                    >
-                      <div className={classNames.playerIndicator}>
-                        Player {(index % 4) + 1}
-                      </div>
-                      {entry.handType === "Pass" ? (
-                        <div className={classNames.passIndicator}>PASS</div>
-                      ) : (
-                        <div className={classNames.playDetails}>
-                          <div className={classNames.playType}>
-                            {entry.handType}
-                          </div>
-                          <div className={classNames.historyCards}>
-                            {entry.cards.map((card, cardIndex) => (
-                              <div
-                                className={classNames.miniCardContainer}
-                                key={cardIndex}
-                                style={{
-                                  marginLeft: cardIndex > 0 ? "-25px" : "0",
-                                }}
-                              >
-                                <Card
-                                  key={cardIndex}
-                                  card={{ ...card, hidden: false }}
-                                />
-                              </div>
-                            ))}
-                          </div>
-                        </div>
-                      )}
-                    </div>
-                  ))}
-                </div>
-              )}
-            </div>
-          )}
+          {/* Use the new HandHistory component */}
+          <HandHistory
+            handHistory={handHistory}
+            currentHandType={currentHandType}
+          />
         </div>
 
         {/* Right player (player 1) */}
